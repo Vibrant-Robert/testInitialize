5773a1b5752a1b57e464ead4e4613a39479c44e6
赵锐华 <zhaoruihua@zhaoruihuas-MacBook-Pro.local>
2022-10-08 11:09:15 +0800

    feat: first commit

---
+++ src/pages/Manage/Menu4/List/index.tsx
@@ -0,0 +1,14 @@
+
+import { PageContainer } from '@ant-design/pro-layout';
+
+const Menu4 = () => {
+  return (
+    <PageContainer>
+     <div>
+      menue4
+     </div>
+    </PageContainer>
+  );
+};
+
+export default Menu4;
